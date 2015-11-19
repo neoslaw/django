2bb711bd64ce23f2495cf36268d999a7646fba78
Damian <neo228wp4@gmail.com>
2015-11-19 22:15:10 +0100

    '9.2'

--- biblio/settings.py
+++ biblio/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    #'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.facebook',
     ########################
     'shelf',
     'contact',
